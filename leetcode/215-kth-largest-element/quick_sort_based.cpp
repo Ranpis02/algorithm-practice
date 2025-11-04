382d6c25bf397fb8fd9318e7b069afc0ef60a27d
Zhihong Xie <ranpis02@gmail.com>
2025-11-04 20:40:02 +0800

    feat: find kth largest element based on quick sort

--- leetcode/215-kth-largest-element/quick_sort_based.cpp
+++ leetcode/215-kth-largest-element/quick_sort_based.cpp
@@ -1,31 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-class QuickSort
+class Solution
 {
 public:
-  void sort(vector<int> nums)
+  int findKthLargest(vector<int> &nums, int k)
   {
+    int res = quickSelect(nums, k);
+
+    return res;
   }
 
 private:
-  void quickSelect(int l, int r, vector<int> arr)
+  int quickSelect(vector<int> &nums, int k)
   {
-    if (l == r)
-      return;
+    vector<int> big, small, equal;
 
-    int i = l + 1;
-    for (int i = l + 1; i <= r; i++)
+    int n = nums.size();
+
+    // Select an element randomly
+    srand(time(0));
+    int pivotIndex = rand() % n;
+
+    int pivot = nums[pivotIndex];
+
+    for (int i = 0; i < n; i++)
     {
-      // if(arr[l])
+      if (nums[i] < pivot)
+        small.push_back(nums[i]);
+      else if (nums[i] > pivot)
+        big.push_back(nums[i]);
+      else
+        equal.push_back(nums[i]);
     }
+
+    if (big.size() >= k)
+      return quickSelect(big, k);
+    else if (big.size() + equal.size() < k)
+      return quickSelect(small, k - big.size() - equal.size());
+    else
+      return pivot;
   }
 };
 
 int main()
 {
+  // new Solution
 
   return 0;
 }
