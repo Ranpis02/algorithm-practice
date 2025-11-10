ff9d528412c2e91cc15c55a348cc1e71a5ed7094
Zhihong Xie <ranpis02@gmail.com>
2025-11-10 21:05:55 +0800

    feat: solve the "detect cycle I & II"

---
+++ leetcode/142-linked-list-cycle-II/base.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution
+{
+public:
+  ListNode *detectCycle(ListNode *head)
+  {
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast && fast->next)
+    {
+      slow = slow->next;
+
+      fast = fast->next->next;
+
+      if (slow == fast)
+        break;
+    }
+
+    if (!fast && !fast->next)
+    {
+      return nullptr;
+    }
+
+    fast = head;
+    while (fast != slow)
+    {
+      fast = fast->next;
+      slow = slow->next;
+    }
+
+    return fast;
+  }
+};
+int main()
+{
+
+  return 0;
+}
