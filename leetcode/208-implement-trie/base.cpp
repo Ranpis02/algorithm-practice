743142d59b8caceecaca122f3f707c8eab9436d0
Zhihong Xie <ranpis02@gmail.com>
2025-11-23 21:02:36 +0800

    feat: solve the "implement trie"

---
+++ leetcode/208-implement-trie/base.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+struct TrieNode
+{
+  bool isEnd = false;
+  TrieNode *children[26] = {nullptr};
+
+  TrieNode() {}
+};
+
+class Trie
+{
+private:
+  TrieNode *root;
+
+public:
+  Trie()
+  {
+    root = new TrieNode();
+  }
+
+  void insert(string word)
+  {
+    TrieNode *node = root;
+
+    for (char ch : word)
+    {
+      int index = ch - 'a';
+      if (!node->children[index])
+        node->children[index] = new TrieNode();
+
+      node = node->children[index];
+    }
+    node->isEnd = true;
+  }
+
+  bool search(string word)
+  {
+    TrieNode *node = root;
+
+    for (char ch : word)
+    {
+      int index = ch - 'a';
+      if (!node->children[index])
+        return false;
+
+      node = node->children[index];
+    }
+
+    return node->isEnd;
+  }
+
+  bool startsWith(string prefix)
+  {
+    TrieNode *node = root;
+
+    for (char ch : prefix)
+    {
+      int index = ch - 'a';
+      if (!node->children[index])
+        return false;
+
+      node = node->children[index];
+    }
+
+    return true;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
