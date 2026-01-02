bd9d3e231f74091384f60dcebf4f4efba8e01c11
Zhihong Xie <ranpis02@gmail.com>
2026-01-02 14:57:15 +0800

    feat: solve the "plus one"

---
+++ leetcode/066-plus-one/base.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  vector<int> plusOne(vector<int> &digits)
+  {
+    int n = digits.size();
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+      if (digits[i] != 9)
+      {
+        digits[i]++;
+        return digits;
+      }
+
+      if (i != 0)
+      {
+        digits[i] = 0;
+      }
+      else
+      {
+        digits[i] = 0;
+        digits.insert(digits.begin(), 1);
+      }
+    }
+
+    return digits;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
