900269136d033681dc4530d5920e46774c3692a0
Zhihong Xie <ranpis02@gmail.com>
2026-01-25 23:18:32 +0800

    feat: solve the "Minimum Difference Between Highest and Lowest of K Scores"

---
+++ leetcode/1984-minimum-difference/base.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int minimumDifference(vector<int> &nums, int k)
+  {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    int ans = nums[k - 1] - nums[0];
+    for (int i = k; i < n; i++)
+    {
+      ans = min(ans, nums[i] - nums[i - k + 1]);
+    }
+
+    return ans;
+  }
+};
+
+int main()
+{
+
+  return 0;
+}
